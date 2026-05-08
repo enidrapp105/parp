f87a6dad1a48ff9d1295a686c7861c9a16eaf063
Enid Rapp <erapp901@gmail.com>
2026-05-07 17:02:34 -0700

    changed command interface and refactored/removed -f option

--- main.cpp
+++ main.cpp
@@ -1,83 +1,99 @@
 #include "parp.h"
 #include "lib/portaudio/src/common/pa_util.h"
+#include <getopt.h>
 #include <string.h>
 
 
 //PULSE_SINK="combined" ./parp -p -f <file_name> 2>/dev/null for virtual mic output
 //or
 //./parp -r -f <file_name>
+static void print_help_message(){
+  printf("usage: parp [-lh][-d <device_number>][-r | -r <record_file_name>][-p <play_file_name>]\n"
+        "-l\tdisplay list of devices\n"
+        "-h\tprint usage info\n"
+        "-d\tspecify device for both input and output\n"
+        "-r\trecord to file(default record_file_name is a.raw)\n"
+        "-p\tplay file (default file is not provided)\n"
+        );
+  exit(1);
+}
 
 int main(int argc, char *argv[]) {
   int opt;
   int selected_device;
-  bool record = false;
-  bool play = false;
   bool list_devices = false;
-  bool provided_file = false;
+  bool file_play = false;
+  bool file_record = false;
   bool spec_device_flag = false;
-  char *file_name = (char *)calloc(MAX_FILE_NAME, sizeof(char));
-  //parp -r <file_name>
-  //parp -p <file_name>
-  while ((opt = getopt(argc, argv, ":f:rphld:")) != -1) {
+  char *file_name_play = (char *)calloc(MAX_FILE_NAME, sizeof(char));
+  char *file_name_record = (char *)calloc(MAX_FILE_NAME, sizeof(char));
+  //TODO:
+  //suggested usage:
+  //parp -r <file_name_record>
+  //parp -p <file_name_play>
+  while ((opt = getopt(argc, argv, ":r:p:hld:")) != -1) {
     switch (opt) {
-    case 'h':
-      printf("usage: parp [-prlh][-d <device_number>][-f <file_name>]\n"
-             "-p\tplay file\n"
-             "-r\trecord to file(default file_name is a.raw)\n"
-             "-l\tdisplay list of devices\n"
-             "-h\tprint usage info\n"
-             "-d\tspecify device for both input and output\n"
-             "-f\tflag to provide name of file to "
-             "record to/play from\n");
+      case 'h':
+        print_help_message();
+      case 'r':
+        if (!file_record) {
+          strncpy(file_name_record, optarg, MAX_FILE_NAME);
+          file_record = true;
+        } else {
+          printf("Multiple files?????\n");
+          exit(1);
+        }
 
-      exit(0);
-    case 'f':
-      if (!provided_file) {
-        strncpy(file_name, optarg, MAX_FILE_NAME);
-        provided_file = true;
-      } else {
-        printf("Multiple files?????\n");
+        break;
+      case 'p':
+        if (!file_play) {
+          strncpy(file_name_play, optarg, MAX_FILE_NAME);
+          file_play = true;
+        } else {
+          printf("Multiple files?????\n");
+          exit(1);
+        }
+
+        break;
+      case 'l':
+        list_devices = true;
+        break;
+      case 'd':
+        if (!spec_device_flag){
+          selected_device = atoi(optarg);
+          spec_device_flag = true;
+        } else {
+          printf("TODO: implement input and output select\n");
+          exit(1);
+        }
+        if(selected_device < 0){
+          printf("ERROR: negative device or invalid device\n");
+        }
+        break;
+      case ':':
+        switch (optopt) {
+            case 'r':
+              file_record = true;
+              strncpy(file_name_record, "a.raw", MAX_FILE_NAME);
+              break;
+            case 'p':
+              printf("please provide a file (.raw)\n");
+              exit(1);
+            case 'd':
+              printf("please provide a device number\n");
+              exit(1);
+        }
+        break;
+      case '?':
+        printf("unkown option: %c\n", optopt);
         exit(1);
-      }
-      break;
-    case 'r':
-      record = true;
-      break;
-    case 'p':
-      play = true;
-      break;
-    case 'l':
-      list_devices = true;
-      break;
-    case 'd':
-      if (!spec_device_flag){
-        selected_device = atoi(optarg);
-        spec_device_flag = true;
-      } else {
-        printf("TODO: implement input and output select\n");
-        exit(1);
-      }
-      if(selected_device < 0){
-        printf("ERROR: negative device or invalid device\n");
-      }
-      break;
-    case ':':
-      printf("please provide a file (.raw) or a device number\n");
-      exit(1);
-    case '?':
-      printf("unkown option: %c\n", optopt);
-      exit(1);
     }
   }
-  if (!provided_file) {
-    strncpy(file_name, "a.raw", MAX_FILE_NAME);
+  if(!file_play && !file_record && !list_devices && !spec_device_flag){
+    print_help_message();
   }
-  if (!provided_file && !record && play) {
-    printf("empty file can't play\n");
-    exit(1);
-  }
-  if (provided_file && play) {
-    if (FILE *file = fopen(file_name, "r")) {
+  if (file_play) {
+    if (FILE *file = fopen(file_name_play, "r")) {
       fclose(file);
     } else {
       printf("ERROR file not found\n");
@@ -90,7 +106,7 @@ int main(int argc, char *argv[]) {
   err = Pa_Initialize();
   checkErr(err);
   paTestData data = {0};
-  memcpy(data.file_name, file_name, MAX_FILE_NAME);
+  memcpy(data.file_name, file_name_record, MAX_FILE_NAME);
   unsigned numSamples;
   unsigned numBytes;
   if (list_devices)
@@ -118,11 +134,12 @@ int main(int argc, char *argv[]) {
   inputParameters.sampleFormat = PA_SAMPLE_TYPE;
   inputParameters.suggestedLatency =
       Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
-  if (record) {
+  if (file_record) {
     RecordSound(inputParameters, &data, err);
   }
   // playback
   memset(&outputParameters, 0, sizeof(outputParameters));
+  memcpy(data.file_name, file_name_play, MAX_FILE_NAME);
   outputParameters.channelCount = 2;
   if(spec_device_flag){
     outputParameters.device = selected_device;
@@ -133,12 +150,14 @@ int main(int argc, char *argv[]) {
   outputParameters.sampleFormat = PA_SAMPLE_TYPE;
   outputParameters.suggestedLatency =
       Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
-  if (play) {
+  if (file_play) {
     PlaySound(outputParameters, &data, err);
   }
   err = Pa_Terminate();
   checkErr(err);
-  free(file_name);
+  free(file_name_play);
+  free(file_name_record);
+
   if (data.ringBufferData)
     PaUtil_FreeMemory(data.ringBufferData);
   printf("\n");
